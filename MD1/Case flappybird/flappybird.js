be90e73d5a4f8f5185e3531004a525ba6408d650
vuluu2412 <fly.soso.high1@gmail.com>
2022-08-28 20:55:52 +0700

    caseflappy

--- MD1/Case flappybird/flappybird.js
+++ MD1/Case flappybird/flappybird.js
@@ -63,9 +63,9 @@ function draw() {
             scor.play();
         }
         // Điều kiện thua game.
-        if (bX + bird.width >= pipe[i].x && bX >= pipe[i].x + pipeNorth.width &&
+        if (bX + bird.width >= pipe[i].x && bX <= pipe[i].x + pipeNorth.width &&
 // so sánh vị trí x của chim với ống.
-            (bY >= pipe[i].y + pipeNorth.height || // Điều kiện chạm ống trên.
+            (bY <= pipe[i].y + pipeNorth.height || // Điều kiện chạm ống trên.
                 bY + bird.height >= pipe[i].y + constant) || // Điều kiện chạm ống dưới
 
             bY + bird.height >= canvas.height - fg.height) // Điều kiện chạm đất.
