55a3d3c05afff2203080614e4fdccaf4166f4170
vuluu2412 <fly.soso.high1@gmail.com>
2022-09-06 21:48:33 +0700

    classcircle

--- MD2/Bai2_OOP/phanloaihinhhoc.js
+++ MD2/Bai2_OOP/phanloaihinhhoc.js
@@ -1,92 +1,53 @@
-class Shape {
-    color:;
-    filled:;
-    constructor(color, filled) {
-        this.color = color;
-        this.filled = filled;
-    }
-}
-
-class Circle {
-    color:
-    radius:
-    filled: boolean
-    constructor(radius: number,
-                color: string,
-                filled: boolean) {
+var Shape = /** @class */ (function () {
+    function Shape(color, filled) {
+    } // dung hon la ghi lai =)) copy la no k
+    return Shape;
+}());
+// loi gi the anh :D 
+var Circle = /** @class */ (function () {
+    function Circle(radius, color, filled) {
         this.color = color;
         this.filled = filled;
         this.radius = radius;
     }
-
-    getArea(): number {
+    Circle.prototype.getArea = function () {
         return this.radius * this.radius * Math.PI;
-    }
-
-    getPerimeter(): number {
+    };
+    Circle.prototype.getPerimeter = function () {
         return 2 * this.radius * Math.PI;
-    }
-}
-let circle = new Circle(3.5, "indigo", false);
-
+    };
+    return Circle;
+}());
+var circle = new Circle(3.5, "indigo", false); // cai loi nay la e co 2 thang let circle nen no bao la declare
 console.log(circle);
-
-class Rectangle {
-    width: number;
-    length: number;
-    color: string;
-    filled: boolean;
-    constructor(width: number,
-                length: number,
-                color: string,
-                filled: boolean) {
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, length, color, filled) {
         this.color = color;
         this.filled = filled;
         this.width = width;
         this.length = length;
     }
-
-    getArea(): number {
+    Rectangle.prototype.getArea = function () {
         return this.width * this.length;
-    }
-
-    getPerimeter(): number {
+    };
+    Rectangle.prototype.getPerimeter = function () {
         return 2 * (this.width + this.length);
-    }
-}
-
-class Square {
-    color: string;
-    filled: boolean
-    side: number
-    constructor(color: string,
-                filled: boolean,
-                side: number) {
+    };
+    return Rectangle;
+}());
+var Square = /** @class */ (function () {
+    function Square(color, filled, side) {
         this.color = color;
         this.filled = filled;
-        this.side = side
+        this.side = side;
     }
-}
-
-let shape = new Shape("red", false);
-
-
+    return Square;
+}());
+var shape = new Shape("red", false); // e doc loi nay no ghi la expected 0 aru co nghia la trong thang Shape cua e phan constructor no k co tham so nhung e lai truyen 2 thang nen no ghi la but got 2 oke anh
 console.log(shape);
-
-
-let circle = new Circle(3.5, "indigo", false);
-
-
+var circle1 = new Circle(3.5, "indigo", false); // hoac la thay ten thang nay la xong
 console.log(circle);
-
-
-let rectangle = new Rectangle(2.5, 3.8, "orange", true);
-
-
+var rectangle = new Rectangle(2.5, 3.8, "orange", true);
 console.log(rectangle);
-
-
-let square = new Square("yellow", true, 5.8);
-
-
+var square = new Square("yellow", true, 5.8);
 console.log(square);
