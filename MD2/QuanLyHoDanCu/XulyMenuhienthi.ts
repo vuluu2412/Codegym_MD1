9447e4aa098bd92f58698b7b5f386f9873bdd883
vuluu2412 <fly.soso.high1@gmail.com>
2022-09-15 17:24:01 +0700

    Quanlykhupho

---
+++ MD2/QuanLyHoDanCu/XulyMenuhienthi.ts
@@ -0,0 +1,23 @@
+// @ts-ignore
+import {NeiBorHood} from "./Class Neiborhood/Class Neiborhood";
+import {MenuHienThi} from "./MenuHienThi";
+
+// @ts-ignore
+let readline= require('readline-sync')
+export class XulyMenuhienthi {
+    // @ts-ignore
+    thongTin = new NeiBorHood();
+
+    disPlayMenu() {
+        MenuHienThi.menu();
+        let choice
+        do {
+            choice = +readline.qution('Nhap lua chon cua ban: ');
+            switch (choice) {
+                case 1: {
+let
+                }
+            }
+        }
+    }
+}
