3fce8319915740e661fc40e66a9946494d03905a
vuluu2412 <fly.soso.high1@gmail.com>
2022-09-26 21:21:15 +0700

    casemodule2

--- MD2/QuanLyTaiLieu/Main.js
+++ MD2/QuanLyTaiLieu/Main.js
@@ -1,26 +1,5 @@
 "use strict";
 exports.__esModule = true;
-// import {ThuVienQuanLy} from "./ThuVienQuanLy";
-// import {Sach} from "./Sach";
-// import {TapChi} from "./TapChi";
-// import {Bao} from "./Bao";
-// import {DanhSachQuanLy} from "./DanhSachQuanLy";
-//
-// let thuvienquanly = new ThuVienQuanLy(1234,"KimDong","500 Ban");
-// let sach1 = new Sach(1111,"KimDong","100","abc",300);
-// let tapchi1 = new TapChi(2222,"adasda","400",1000,12);
-// let bao1 = new Bao(55555,"qqeqeqe","100000","11/11/2020");
-//
-//
-// let danhsachquanly:DanhSachQuanLy = new DanhSachQuanLy();
-//
-// danhsachquanly.addDanhSach(sach1);
-// danhsachquanly.addDanhSach(thuvienquanly);
-// danhsachquanly.addDanhSach(tapchi1);
-// danhsachquanly.addDanhSach(bao1);
-// danhsachquanly.findTaiLieu(1111);
-// console.log(danhsachquanly.deleteDanhSach(55555));
-// console.log(danhsachquanly.showTaiLieu());
 var XulyMenu_1 = require("./XulyMenu");
 var xl = new XulyMenu_1.XuLyMenu();
 xl.handleShowMenu();
